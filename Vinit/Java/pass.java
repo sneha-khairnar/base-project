480b079d95e0ace2bd46ca4246f64d84273cdeb1
Vinit Kshirsagar <vinitkshirsagar18@gmail.com>
2025-02-07 22:53:06 +0530

    pass.java

--- Vinit/Java/pass.java
+++ Vinit/Java/pass.java
@@ -1,1 +1,20 @@
+import java.util.Scanner;
 
+class pass {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter sing-in password: ");
+        String log = scanner.nextLine();
+       
+                for(int i = 0; i < 3; i++){
+                System.out.print("Enter password: ");
+                String pass = scanner.nextLine();
+                if(pass.equals(log)){
+                    System.out.println("Right password");
+                    break;
+              System.out.println("Wrong passoword"); 
+            
+            }
+        }
+    }
+}
