8c105f0b0e78935c8fa88bb7fc64c561746ab200
Vinit-Kshirsagar <vinitkshirsagar18@gmail.com>
2025-02-16 10:02:44 +0530

    Practicing JS
    Loops, Functions, Arrays, and Objects.

--- Vinit/JavaScript/test.js
+++ Vinit/JavaScript/test.js
@@ -5,5 +5,39 @@ var a = "car"
 console.log(a);
 console.log('Making changes');
 var b = 'sports'
-console.log(b + " "+a);
+// console.log(b + " "+a);
 
+// Array
+let arr = [1,23,34,45,56];
+console.log(arr[1]);
+
+// Object
+let obj = {
+    
+    type: 'sports car',
+    name: 'porshce',
+    model: '911 RSR',
+    year: 2018,
+}
+console.log(obj.year);
+
+// Loop (for)
+for(i = 8; i < 11; i++){
+    console.log(i +=i);
+}
+
+// Loop (while)
+var a = 8;
+while(a > 0 && a < 10){
+    a++;
+    console.log('I is greater than 0' + a);
+}
+
+// Functions
+  let YourName; //= prompt('Enter your name');
+function fig(YourName){
+    console.log(YourName);
+}
+fig(YourName);
+
+ 
