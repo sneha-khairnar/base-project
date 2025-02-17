46dc6c4b812c4e1c70525025518d430a925c1a27
Vinit-Kshirsagar <vinitkshirsagar18@gmail.com>
2025-02-17 23:37:48 +0530

    Update in calculator
    Gets operator from user, uses arrow function and object to perform and print output.

--- Vinit/JavaScript/cal.js
+++ Vinit/JavaScript/cal.js
@@ -1,5 +1,21 @@
 alert('This is a calculator made using JS');
-let a = parseInt(prompt('Enter any number'));
-let b = parseInt(prompt('Enter any number'));
+let num1 = parseInt(prompt('Enter any number'));
+let num2 = parseInt(prompt('Enter any number'));
+let operator = prompt('Enter any operator');
 
-alert(a + b);
+
+const operation = {
+    "+": (a,b) => a + b,
+    "-": (a,b) => a - b,
+    "*": (a,b) => a * b,
+    "/": (a,b) => a / b,
+};
+
+let result = operation[operator] ? operation[operator](num1, num2) : "Invalid operator!";
+
+//  here operator holds the operator value ("=", "-", "*", "/") and then operation accesses it by 
+// operation[operator] and gives it to the function (a, b), a and b as num1 and num2
+// ? and : works as if-else, if operatation from operator, use it in function (a,b) to perform on num1 num2
+
+console.log("Result: ", result);
+alert("Result: " + result);
