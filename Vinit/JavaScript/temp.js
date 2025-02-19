ed2832115419d0832b5be5eaab3a4fa9ec1733f8
Vinit-Kshirsagar <vinitkshirsagar18@gmail.com>
2025-02-19 10:51:35 +0530

    temp.js works
    Can convert temperature from Celcuis to Fahrenheit

--- Vinit/JavaScript/temp.js
+++ Vinit/JavaScript/temp.js
@@ -1,1 +1,5 @@
-console.log("print something");
+alert('This is a program to convert temperature from Celcius to Fahrenheit');
+let temp = parseInt(prompt("Enter temperature in Celcius"));
+let far;
+far = (9/5 * temp) + 32;
+alert(far);
