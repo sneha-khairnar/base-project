808e3b7a144b2828b5f32f4e734e0015dac315f2
Vinit-Kshirsagar <vinitkshirsagar18@gmail.com>
2025-02-19 10:44:36 +0530

    Major changes for Teml.js

--- Vinit/JavaScript/temp.js
+++
@@ -1,6 +0,0 @@
-alert('Program to convert temperature from Celcius to Farhenheit');
-let temp = parseInt(prompt('Enter temperature in Celcius'));
-let far;
-far = (9/5 * temp) + 32;
-alrt(far);
-console.log('Program to convert temperature');
