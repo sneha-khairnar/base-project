ba3923e18ddbdc3edd9db1cdfcfced9c2483d9e0
Vinit-Kshirsagar <vinitkshirsagar18@gmail.com>
2025-02-22 10:32:24 +0530

    Updating temp.js

--- Vinit/JavaScript/temp.js
+++ Vinit/JavaScript/temp.js
@@ -1,4 +1,4 @@
-alert('This is a program to convert temperature from Celcius to Fahrenheit');
+alert('This is a program to convert temperature from Celsius to Fahrenheit');
 let temp = parseInt(prompt("Enter temperature in Celcius"));
 let far;
 far = (9/5 * temp) + 32;
