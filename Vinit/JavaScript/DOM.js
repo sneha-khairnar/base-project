c0aa06cad093425f73c104444ff6b140ab12df33
Aniket793 <vinitkshirsagar18@gmail.com>
2025-03-21 23:28:10 +0530

    Added colors to the counter.
    Green counter represent positive counter, red represents negative counter, and black represent zero counter.

--- Vinit/JavaScript/DOM.js
+++ Vinit/JavaScript/DOM.js
@@ -4,16 +4,35 @@ document.addEventListener("DOMContentLoaded", function () {
 const counterDisplay = document.getElementById  ("counter");
 const incrementBtn = document.getElementById ("increment");
 const decrementBtn = document.getElementById ("decrement");
+const resetBtn = document.getElementById("reset");
 
 let count = 0;
+
+function updateCounterColor(){
+ if(count > 0){
+    counterDisplay.style.color = "green";
+ }
+ else if (count < 0) {
+    counterDisplay.style.color = "red";
+ } else  {
+    counterDisplay.style.color = "black";
+ }
+}
 incrementBtn.addEventListener("click", function(){
     count++;
     counterDisplay.textContent = count;
+    updateCounterColor();
 });
 
 decrementBtn.addEventListener("click", function(){
     count--;    
     counterDisplay.textContent = count;
+    updateCounterColor();
 });
+
+resetBtn.addEventListener("click", function(){
+    count = 0;
+    counterDisplay.textContent = count;
+})
 
 });
